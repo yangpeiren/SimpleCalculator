0f3702329cd534e8dfd86a32b3d79638e3b4f5dc
Peiren Yang <yangpeiren@163.com>
2017-11-11 23:04:46 +0100

    Finished first prototype

--- SimpleCalculator/SimpleCalculator/SimpleCalculator.iOS/obj/iPhoneSimulator/Debug/SimpleCalculator.iOS.D_.My_projects.SimpleCalculator.SimpleCalculator.SimpleCalculator.SimpleCalculator.MainPage.xaml.g.cs
+++ SimpleCalculator/SimpleCalculator/SimpleCalculator.iOS/obj/iPhoneSimulator/Debug/SimpleCalculator.iOS.D_.My_projects.SimpleCalculator.SimpleCalculator.SimpleCalculator.SimpleCalculator.MainPage.xaml.g.cs
@@ -16,8 +16,12 @@ namespace SimpleCalculator {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label outText;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            outText = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "outText");
         }
     }
 }
--- SimpleCalculator/SimpleCalculator/SimpleCalculator/MainPage.xaml.cs
+++ SimpleCalculator/SimpleCalculator/SimpleCalculator/MainPage.xaml.cs
@@ -9,9 +9,94 @@ namespace SimpleCalculator
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        private string[] operands = new string[2];
+        private string _operator;
+        private Label calculatorText;
+
+        public MainPage()
 		{
 			InitializeComponent();
+            this.calculatorText = outText;
 		}
-	}
+
+        public void OnButtonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if ("0123456789.".Contains(button.Text))
+                AddDigitOrDecimalPoint(button.Text);
+            else if ("÷×+-".Contains(button.Text))
+                AddOperator(button.Text);
+            else if ("=" == button.Text)
+                Calculate();
+            else
+                Reset();
+        }
+
+        private void Reset()
+        {
+            operands[0] = operands[1] = null;
+            _operator = null;
+            UpdateCalculatorText();
+        }
+
+        private void Calculate(string newOperator = null)
+        {
+            double? result = null;
+            double? first = operands[0] == null ? null : (double?)double.Parse(operands[0]);
+            double? second = operands[1] == null ? null : (double?)double.Parse(operands[1]);
+
+            switch (_operator)
+            {
+                case "÷":
+                    if(second != 0)
+                        result = first / second;
+                    break;
+                case "×":
+                    result = first * second;
+                    break;
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+            }
+
+            if (result != null)
+            {
+                operands[0] = result.ToString();
+                _operator = newOperator;
+                operands[1] = null;
+                UpdateCalculatorText();
+            }
+        }
+
+        private void AddOperator(string text)
+        {
+            if (operands[1] != null)
+            {
+                Calculate(text);
+                return;
+            }
+
+            _operator = text;
+
+            UpdateCalculatorText();
+        }
+
+        private void AddDigitOrDecimalPoint(string text)
+        {
+            int index = _operator == null ? 0 : 1;
+
+            if (text == "." && operands[index].Contains("."))
+                return;
+
+            operands[index] += text;
+
+            UpdateCalculatorText();
+        }
+
+        private void UpdateCalculatorText() => calculatorText.Text = $"{operands[0]} {_operator} {operands[1]}";
+    }
+
 }
