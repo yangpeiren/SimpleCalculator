984d34ed9c710e590ac70979972a256eb5182744
Peiren Yang <yangpeiren@163.com>
2017-11-24 18:45:28 +0100

    add windows forms

---
+++ SimpleCalculator/SimpleCalculator.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleCalculator.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleCalculator.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7e5df519-59ad-41c3-ae84-6f7755ce9d0d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleCalculator/SimpleCalculator.UnitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.UnitTest
+{
+    [TestClass]
+    public class MainPageUnitTest
+    {
+        [TestMethod]
+        public void CalculateTest()
+        {
+            //Add Unit test for the program logic
+            // Not enougth space on C disk, no Windows API installed, therefore
+            // not possible to test the SimpleCalculator project
+        }
+    }
+}
