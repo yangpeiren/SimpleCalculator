d0f93b933838b60eac2dd76af07c5aac44a913a2
Peiren Yang <yangpeiren@163.com>
2017-11-28 00:26:22 +0100

    try to fix problem with build on test cloud

--- SimpleCalculator/SimpleCalculator.UITest/Tests.cs
+++ SimpleCalculator/SimpleCalculator.UITest/Tests.cs
@@ -8,7 +8,7 @@ using Xamarin.UITest.Queries;
 namespace SimpleCalculator.UITest
 {
     [TestFixture(Platform.Android)]
-    [TestFixture(Platform.iOS)]
+    //[TestFixture(Platform.iOS)]
     public class Tests
     {
         IApp app;
